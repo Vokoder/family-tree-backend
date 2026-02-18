4a338ac263c8146c55195c50721ff5ac6b0d2935
Domanskij Dmitry <molohko12@bk.ru>
2026-02-19 00:02:04 +0300

    Added swagger

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,4 +1,5 @@
 import env from 'env-var'
+import type { ServiceAccount } from 'firebase-admin';
 
 export const PORT: number = env.get('PORT').required().asIntPositive();
 export const CORS_ORIGIN: string = env.get('CORS_ORIGIN').required().asString();
@@ -8,3 +9,9 @@ export const FRONT_ADRESS: string = env.get('FRONT_ADRESS').required().asString(
 export const QUERIES_CACHE_TIME_SEC: number = env.get('QUERIES_CACHE_TIME_SEC').required().asIntPositive();
 
 export const JWT_SECRET: string = env.get('JWT_SECRET').required().asString();
+
+export const FIREBASE_SERVICE_ACCOUNT: ServiceAccount = {
+  projectId: env.get('FIREBASE_PROJECT_ID').required().asString(),
+  privateKey: env.get('FIREBASE_PRIVATE_KEY').required().asString(),
+  clientEmail: env.get('FIREBASE_CLIENT_EMAIL').required().asString(),
+};
--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,22 @@ import { CORS_ORIGIN, PORT } from '#app.config.ts';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { errorMiddleware, httpErrorMiddleware, typeErrorMiddleware } from '#shared/middleware/errors.middleware.ts';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+// Swagger configuration
+const schemaPath = path.join(__dirname, '../docs/openapi.yml');
+const openapi = YAML.load(schemaPath);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapi));
+app.get('/openapi.json', (_req, res) => res.json(openapi));
 
 // Middlewares
 app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
